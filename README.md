7f53781a259f508f732cf784c62b383fbe7afdf3
747hzx <59676354+0xhzx@users.noreply.github.com>
2024-02-11 20:59:54 +0000

    test lambda

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 # rust-new-project-template
 A good starting point for a new Rust project
 
+## Preparation
+1. Prepare for the environment.
+```shell
+sudo apt install python3-pip
+python3 -m venv venv
+source ./venv/bin/activate
+```
+2. PiP on any system with Python 3 installed
+`pip3 install cargo-lambda`
+
+* [See other installation options](https://github.com/awslabs/aws-lambda-rust-runtime)
+
+
 ## References
 
 * [rust-cli-template](https://github.com/kbknapp/rust-cli-template)
---
+++ cars-lambda/src/lib.rs
@@ -0,0 +1,63 @@
+/*
+ * This program reads the Iris dataset from a CSV file located at "src/data/iris.csv".
+ * It assumes the CSV has headers.
+ *
+ * 1. Filtering:
+ *    It filters the rows where the column 'sepal_length' is greater than 5.0.
+ *
+ * 2. Grouping:
+ *    The program then groups the data by the 'species' column.
+ *
+ * 3. Aggregation:
+ *    For each group (species in this case), it calculates the sum of the other columns.
+ *
+ * Output Explanation:
+ *    The resulting DataFrame has a shape of (3, 5), indicating that it has 3 rows and 5 columns.
+ *    The rows represent the three species of Iris flowers: 'setosa', 'versicolor', and 'virginica'.
+ *    The columns represent:
+ *    - 'species': The name of the Iris species
+ *    - 'sepal_length': The sum of 'sepal_length' values for each species
+ *    - 'sepal_width': The sum of 'sepal_width' values for each species
+ *    - 'petal_length': The sum of 'petal_length' values for each species
+ *    - 'petal_width': The sum of 'petal_width' values for each species
+ *
+ *    For example, for 'virginica', the sum of 'sepal_length' values is 324.5,
+ *    the sum of 'sepal_width' values is 146.2, and so on.
+ */
+
+// Import necessary modules from the `polars` crate
+use polars::prelude::*;
+use std::io::Cursor;
+
+// Car dataset in CSV format
+const CAR_DATA: &str = "brand,model,year,price
+Toyota,Camry,2018,25000
+Honda,Accord,2019,27000
+Toyota,Corolla,2017,20000
+Ford,Fusion,2018,22000
+Honda,Civic,2020,28000
+Ford,Focus,2019,21000
+Toyota,Rav4,2020,30000
+Honda,CRV,2018,31000
+Ford,Escape,2020,32000";
+
+pub fn calculate(filter_value: f64) -> Result<DataFrame, PolarsError> {
+    let file = Cursor::new(CAR_DATA);
+
+    let df = CsvReader::new(file)
+        .has_header(true)
+        .finish()?
+        .lazy()
+        // add filter
+        .filter(col("price").gt(lit(filter_value)))
+        .groupby(vec![col("brand")])
+        .agg(&[
+            col("price").mean().alias("average_price"),
+            col("year").mean().alias("average_year"),
+        ])
+        .collect()?;
+
+    Ok(df)
+}
+
+
